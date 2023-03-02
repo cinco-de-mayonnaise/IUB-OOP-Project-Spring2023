2eaca2362009ba2b0dae6c9aace1bb0e5654b7f0
AbdullahTrees <124304231+cinco-de-mayonnaise@users.noreply.github.com>
2023-03-02 22:40:04 +0600

    Finalizing Project Skeleton

---
+++ ProjectBritishCouncil/src/FXMLScenes/LogonUIController.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package FXMLScenes;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author Abdullah
+ */
+public class LogonUIController implements Initializable {
+
+    @FXML
+    private Label Label_Forgot_Password;
+    @FXML
+    private TextField TF_EmailID;
+    @FXML
+    private PasswordField PasswordField_AccPassword;
+    @FXML
+    private Button Btn_Login;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @FXML
+    private void click_Forgot_Password(MouseEvent event) {
+    }
+
+    @FXML
+    private void click_Login(MouseEvent event) {
+    }
+    
+}
