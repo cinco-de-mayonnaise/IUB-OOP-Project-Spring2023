ad58b8763fd9b8690cc15f62fccbd4a11f482e16
Protik <2022133@iub.edu.bd>
2023-05-04 22:52:29 +0600

    IELTSCandidateDashboardController text area

--- ProjectBritishCouncil/src/FXMLScenes/Users/protik/IELTSCandidate/IELTSCandidateDashboardController.java
+++ ProjectBritishCouncil/src/FXMLScenes/Users/protik/IELTSCandidate/IELTSCandidateDashboardController.java
@@ -1,12 +1,19 @@
 package FXMLScenes.Users.protik.IELTSCandidate;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import projectbritishcouncil.common.util.SceneSwitcher;
 
@@ -18,14 +25,14 @@ import projectbritishcouncil.common.util.SceneSwitcher;
 public class IELTSCandidateDashboardController implements Initializable {
 
     @FXML
-    private Button logoutfxId;
-
-    /**
-     * Initializes the controller class.
-     */
+    private TextArea statusfxid;
+    @FXML
+    private Label welcomeLabel;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
     }    
 
     @FXML
