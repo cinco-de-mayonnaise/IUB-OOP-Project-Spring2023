9c79296a3fcd52399b19687ea31a76845e2129cb
sopen0088@gmail.com <126973261+MohdSopen@users.noreply.github.com>
2023-05-05 01:41:21 +0600

    Final
    Final

--- ProjectBritishCouncil/src/FXMLScenes/Users/sopen/InvigilatorDeshboardController.java
+++ ProjectBritishCouncil/src/FXMLScenes/Users/sopen/InvigilatorDeshboardController.java
@@ -7,7 +7,10 @@ package FXMLScenes.Users.sopen;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import projectbritishcouncil.common.util.SceneSwitcher;
 
 /**
  * FXML Controller class
@@ -23,5 +26,45 @@ public class InvigilatorDeshboardController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+    private void invigilationtimeOnClick(ActionEvent event) {
+        SceneSwitcher.createStagewithScene("/FXMLScenes/Users/sopen/Invigilator/ChooseInvigilatingTime.fxml", false);
+    }
+    
+    @FXML
+    private void attendanceOnClick(ActionEvent event) {
+        SceneSwitcher.createStagewithScene("/FXMLScenes/Users/sopen/Invigilator/Attendance.fxml", false);
+    }
+    
+    @FXML
+    private void itsupportOnClick(ActionEvent event) {
+        SceneSwitcher.createStagewithScene("/FXMLScenes/Users/sopen/Invigilator/ITSupport.fxml", false);
+    }
+    
+    @FXML
+    private void leaveapplicationOnClick(ActionEvent event) {
+        SceneSwitcher.createStagewithScene("/FXMLScenes/Users/sopen/Invigilator/LeaveApplication.fxml", false);
+    }
+    
+    @FXML
+    private void observationreportOnClick(ActionEvent event) {
+        SceneSwitcher.createStagewithScene("/FXMLScenes/Users/sopen/Invigilator/ChooseInvigilatingTime.fxml", false);
+    }
+    
+    @FXML
+    private void checkstatusOnClick(ActionEvent event) {
+        SceneSwitcher.createStagewithScene("/FXMLScenes/Users/sopen/Invigilator/CheckStatus.fxml", false);
+    }
+    
+    @FXML
+    private void examscheduleOnClick(ActionEvent event) {
+        SceneSwitcher.createStagewithScene("/FXMLScenes/Users/sopen/Invigilator/ExaminationSchedule.fxml", false);
+    }
+    
+    @FXML
+    private void reportstudentOnClick(ActionEvent event) {
+        SceneSwitcher.createStagewithScene("/FXMLScenes/Users/sopen/Invigilator/ReportCandidate.fxml", false);
+    }
     
 }
